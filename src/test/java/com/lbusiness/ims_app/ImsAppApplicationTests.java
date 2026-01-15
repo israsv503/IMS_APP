8e13b6f4e68108840e6432b0db13ddb280f10958
LS24009-LS <ls24009@ues.edu.sv>
2026-01-15 16:24:53 -0600

    chore: initial project bootstrap from spring initializr

---
+++ src/test/java/com/lbusiness/ims_app/ImsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbusiness.ims_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
